c8aae3b902cde8c0bcad1e4a16ba33e33597dac4
20Singh <kritikasingh2315@gmail.com>
2022-03-04 18:50:17 +0530

    changes

---
+++ script.js
@@ -0,0 +1,76 @@
+// if..else
+// let a = 10;
+
+// if (a == 10){
+//     //success
+// } else {
+//     //failure
+// }
+
+
+// let a = -10;
+
+// if (a < 10){
+//     console.log("Negative");
+// } else if (a == 0) {
+//     console.log("Zero");
+// } else {
+//     console.log("Positive");
+// }
+
+
+
+
+// let gender = "m";
+// switch(gender){
+//     case "m":
+//         console.log("Male");
+//         break;
+//     case "m":
+//         console.log("female");
+//         break; 
+//     case "m":
+//         console.log("transgender");
+//         break;   
+// }
+
+
+
+
+// let n=7;
+// switch(n){
+//     case 0 :
+//         console.log("Today is Monday");
+//         break;
+//     case 1 :
+//         console.log("Today is Tuesday");
+//         break;
+//     case 2 :
+//             console.log("Today is wednesday");
+//             break;
+//     case 3 :
+//         console.log("Today is Thursday");
+//         break;
+//     case 4 :
+//             console.log("Today is Friday");
+//             break;
+//     case 5 :
+//         console.log("Today is Saturday");
+//         break;
+//     case 6:
+//             console.log("Today is Sunday");
+//         break;
+//     default:
+//         console.log("No weekdays");
+// 
+//console.log("19*"+i+"=" + 19*i);
+//  let a = 19;
+//  let b;
+
+//  for( let i = 1; i <= 10; i++ ) {
+//         b = a * i;
+//  console.log( a+ "*" +i+ "=" +b );
+//  }
+  
+for(let i=1; i<=10 ; i++){
+  console.log( "10*" +i+ "=" +10*i );}
